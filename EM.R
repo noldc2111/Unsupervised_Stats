1ddd4c877ff6e9bc5d3f90503bf633d2371ae41f
Casey Nold <caseyjnold@gmail.com>
2017-03-20 02:41:26 -0700

    Updated the update math in the E/M steps

--- EM.R
+++ EM.R
@@ -1,10 +1,15 @@
 library(grid)
 library(png)
 library(MASS)
+library(mvtnorm)
 `%+=%` = function(e1,e2) eval.parent(substitute(e1 <- e1 + e2))
 
 # read in data file 
-d = read.csv("/Users/caseynold/Desktop/STAT610/Unsupervised/kmeans/data/training_ss_151_blocks.csv")
+d = read.csv("./BrunoData/training_ss_151_blocks.csv")
+means = read.csv("/Users/caseynold/Desktop/STAT610/Unsupervised/kmeans/kmeans.csv")
+r = read.csv("/Users/caseynold/Desktop/STAT610/Unsupervised/kmeans/ri.csv")
+r = r[-1]
+means = means[-1]
 #remove the index from the image
 d = as.matrix(d[,-1])
 #number of samples
@@ -14,18 +19,44 @@ p = dim(d)[2]
 # number of clusers
 k = 3
 # create responsibility matrix
-ri = matrix(rnorm(3, mean=0.5,sd=0.5),nrow=n,ncol=k)
+ri = matrix(0,nrow=n,ncol=k)
+#r = matrix(unlist(r),ncol=k,byrow=TRUE)
 
 # parameter matrices 
-#mu = matrix(rnorm(k*p, mean=0.5,sd=0.5),nrow=k,ncol=p ) 
-mu = matrix(c((1/255),(128/255),(220/255)),nrow=k,ncol=p ) 
-sigma = matrix(rnorm(3, mean=0.5,sd=0.5),nrow=k,ncol=p ) 
-pi = matrix(rnorm(3,mean=0.5,sd=0.5), nrow = k,ncol=1 ) # was p
+mu = matrix(unlist(means),ncol=p,byrow=TRUE)
+sigma1 = diag(rnorm(1, mean=0.5,sd=0.5),nrow=p,ncol=p ) 
+sigma2 = diag(rnorm(1, mean=0.5,sd=0.5),nrow=p,ncol=p ) 
+sigma3 = diag(rnorm(1, mean=0.5,sd=0.5),nrow=p,ncol=p ) 
+
+SIGMA = list(sigma1,sigma2,sigma3)
+pi = matrix(c(.33,.34,.33), nrow = k,ncol=1 ) # was p
 
 m = matrix(0,nrow=k,ncol=p)
-#stochastic row matrix
-pi = pi/rowSums(pi)
+############# Start Initialization ###################
 
+rk = colSums(r)
+# update pi
+pi = rk/n  #notes  02/FEB/2017
+mu_old = mu
+# Initialize mu
+
+for(i in (1:k)){ 
+  dv_mu = 0
+  for(j in (1:n)){
+    dv_mu %+=% ((r[j,i]) %*% d[j,])
+  }
+  mu[i,] = (dv_mu/rk[i])
+}
+
+dv_sig = matrix(0,p,p)
+#Initialize sigma
+for(i in 1:k){
+  for(j in 1:n){
+    dv_sig = dv_sig + (r[j,i]* ((d[j,] - mu_old[i,])%*%t(d[j,] - mu_old[i,])))
+  }
+  SIGMA[[i]] = (dv_sig/rk[i])
+}
+####### End Initialization ########
 
 #Repeat
 step = 0
@@ -33,14 +64,23 @@ repeat{
   step = step+1
   
   #expectation: 1.) compute responsibilities
+ 
+  ri_old = ri
+  
   for(i in 1:k){
-    #ri[,i] = pi[i] %*% dnorm(d[i,],mu[i],sd=sqrt(abs(sigma[i])),log=F)
-    #ri[,i] = pi[i] %*% rnorm(d[i,],mu[i],sd=sqrt(abs(sigma[i])))
-    ri[,i] = pi[i] %*% mvrnorm(1,mu[i],sigma[i])
-    #ri[,i] = ri_temp/rowSums(ri_temp)
+    for(j in 1:n){
+      sumProb =(pi[1]*dmvnorm(d[j,],mu[1,],SIGMA[[1]]) +
+                pi[2]*dmvnorm(d[j,],mu[2,],SIGMA[[2]]) +
+                pi[3]*dmvnorm(d[j,],mu[3,],SIGMA[[3]]) )
+      if(sumProb == 0){
+        ri[j,i] = ri_old[j,i]
+      }else{
+        ri[j,i] = (pi[i] * dmvnorm(x =d[i,],mean=mu[i,],sigma=SIGMA[[i]]))/sumProb
+      }
+    }
   }
 
-  ri = ri/rowSums(ri)
+  #ri = ri/rowSums(ri)
   #print(ri)
 
   #maximazation
@@ -49,22 +89,31 @@ repeat{
   # update pi
   pi = rk/n  #notes  02/FEB/2017
   
+  mu_old = mu
   # update mu
-  dv_mu = 0
   for(i in (1:k)){ 
+    dv_mu = 0
     for(j in (1:n)){
-      dv_mu %+=% (ri[j,i]) * d[j,]
+      dv_mu %+=% ((ri[j,i]) * d[j,])
     }
-    mu[i] = (dv_mu/rk[i])
-   }
+   if(rk[i]==0){
+      mu[i,] = dv_mu
+    }else{
+      mu[i,] = (dv_mu/rk[i])
+    }
+  }
   
-  dv_sig = 0
   #update sigma
   for(i in 1:k){
+    dv_sig = matrix(0,p,p)
     for(j in 1:n){
-      dv_sig %+=% ri[j,i]* (d[j,] - mu[i,])^2
+      dv_sig %+=% ri[j,i]* ((d[j,] - mu[i,])%*%t(d[j,] - mu[i,]))
     }
-    sigma[i] = (dv_sig/rk[i])
+    if(rk[i] == 0){
+      SIGMA[[i]] = 0
+    }else{
+      SIGMA[[i]] = (dv_sig/rk[i])
+    }
   }
   
   if(step == 5){
